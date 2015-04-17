3f0cb7677b412affea8dfc666616190f7b5bb3ef
Erik <erik.d.stock@gmail.com>
2015-04-17 14:03:17 -0500

    added survey and index routes

---
+++ app/controllers/survey.rb
@@ -0,0 +1,39 @@
+get '/surveys' do
+  @surveys = Survey.all
+  erb :'/surveys'
+end
+
+get '/surveys/new' do
+  erb :'/surveys/new'
+end
+
+post '/surveys' do
+  survey = Survey.new(params[:survey])
+  if survey.save
+    redirect '/surveys'
+  else
+    @errors = survey.errors.full_messages.to_sentence
+    erb :'/surveys/new'
+  end
+end
+
+get '/surveys/:id' do
+  @survey = Survey.find(params[:id])
+  erb :'/surveys/show'
+end
+
+# get '/surveys/:id/edit' do
+#   @survey = Survey.find(params[:id])
+#   erb :'/surveys/edit'
+# end
+
+# put '/surveys/:id' do
+#   @survey = Survey.find(params[:id])
+#   if @survey.update(params[:survey])
+#     redirect '/'
+
+# end
+
+# delete '/surveys/:id' do
+
+# end
