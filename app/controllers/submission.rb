1b1995bb2b7aab8cdbd6ee470574438a5eb3a564
Erik <erik.d.stock@gmail.com>
2015-04-18 12:00:12 -0500

    finished answer views

--- app/controllers/submission.rb
+++ app/controllers/submission.rb
@@ -1,10 +1,21 @@
-#NEW
-# get '/surveys/:id/submissions' do
-#   @submission = Submission.create
-#   redirect "/submissions/#{@submission.id}"
-# end
+get '/surveys/:id/submissions' do
+	"this is a submission for survey #{params[:id]}"
+	@survey =	Survey.find(params[:id])
+	@questions = @survey.questions
+  @submission = Submission.create
+ erb :'submissions/new'
+end
 
-# get '/submissions/:id'
-#   @submission.find(params[:id])
-#   erb :'submission/new'
-# end
+post '/submissions/:id' do
+	@submission = Submission.find(params[:id])
+	if request.xhr?
+		puts "ill do this later"
+	else
+		params[:answers].each do |question, answer|
+	  
+	  @submission.responses <<  Response.create!(submission_id: @submission.id, choice_id: answer)
+
+		end
+		redirect '/'
+ 	end
+end
