41ee8169f86d67a9406f1de80c96951d4e88070f
Erik <erik.d.stock@gmail.com>
2015-04-18 15:46:06 -0500

    created part of the submission show erb

--- app/controllers/submission.rb
+++ app/controllers/submission.rb
@@ -2,17 +2,28 @@ get '/surveys/:id/submissions' do
 	"this is a submission for survey #{params[:id]}"
 	@survey =	Survey.find(params[:id])
 	@questions = @survey.questions
-  @submission = Submission.create
+  @submission = Submission.create(responder_id: session[:id], survey_id: @survey.id)
  erb :'submissions/new'
+end
+
+
+get '/submissions/:id' do
+	@submission = Submission.find(params[:id])
+	@responses = @submission.responses
+	@survey = @submission.survey
+	@questions = @survey.questions
+	erb :'/surveys/show'
 end
 
 post '/submissions/:id' do
 	@submission = Submission.find(params[:id])
+	@responses = @submission.responses
+	@survey = @submission.survey
+	@questions = @survey.questions
 	if request.xhr?
 		puts "ill do this later"
 	else
 		params[:answers].each do |question, answer|
-	  
 	  @submission.responses <<  Response.create!(submission_id: @submission.id, choice_id: answer)
 
 		end
