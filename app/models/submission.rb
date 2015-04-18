bd5e61c0587d1c47d725dab72d9e016e9b40e1c9
Erik <erik.d.stock@gmail.com>
2015-04-18 18:22:41 -0500

    survey show done

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,6 @@ class Submission < ActiveRecord::Base
 	belongs_to :responder, class_name: "User"
 	belongs_to :survey
 	has_many :responses
-	# has_many :choices, through: :responses
-	# has_many :questions, through: :choices
+	has_many :choices, through: :responses
+	has_many :questions, through: :choices
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-User.destroy_all
-User.create!(username: "ryan", email: "ryan@test.com", password: "123456")
+# User.destroy_all
+# User.create!(username: "ryan", email: "ryan@test.com", password: "123456")
 
-Survey.create!(title: "newsurvey")
+# Survey.create!(title: "newsurvey")
 
-Question.create!(prompt: 'Do you like pizza?', survey_id: 1)
+# Question.create!(prompt: 'Do you like pizza?', survey_id: 1)
 
-Choice.create!(body: 'Agree', question_id: 1)
-Choice.create!(body: 'Disagree', question_id: 1)
+# Choice.create!(body: 'Agree', question_id: 1)
+# Choice.create!(body: 'Disagree', question_id: 1)
 
-Question.create!(prompt: 'Do you have any swag to spare?', survey_id: 1)
+# Question.create!(prompt: 'Do you have any swag to spare?', survey_id: 1)
 
-Choice.create!(body: 'Agree', question_id: 2)
-Choice.create!(body: 'Disagree', question_id: 2)
+# Choice.create!(body: 'Agree', question_id: 2)
+# Choice.create!(body: 'Disagree', question_id: 2)
