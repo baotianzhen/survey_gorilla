0802e10d27ade5d7108889ac45cd9d9c3fb915d3
Erik <erik.d.stock@gmail.com>
2015-04-17 20:05:38 -0500

    add login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,31 @@
 class User < ActiveRecord::Base
 	has_many :submissions
 	has_many :surveys, foreign_key: :author_id
+
+	validates :username, presence: true, uniqueness: true
+	validates :email, presence: true, uniqueness: true, format: {with: /^[\w\d.]+@[\w\d]+.[\w\d]+/, message: "must be a valid email address"}
+	validates :form_password, presence: true, format: {with: /[\w\d]{6,}/}
+
+	include BCrypt
+
+
+
+
+	def password=(pass)
+		@form_password = pass
+		@password = Password.create(pass)
+		self.password_hash = @password
+	end
+
+	def password
+		@password ||= Password.new(password_hash)
+	end
+
+	def self.authenticate(email, password)
+		user = User.find_by_email(email)
+		return user if user && (user.password == password)
+		nil
+	end
+
+
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+	$(document).on("click", "#new-user", function(e) {
+		e.preventDefault();
+		$("#login-popup").removeClass("hide");
+		$("#login-popup").addClass("hud-popup");
+})
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
